fe8a775bd6e13850728b78afabe66cfe13cc5e23
michaelrlas <michaelsilvamkt@gmail.com>
2022-07-06 19:22:34 -0300

    adicionada as aulas 05-07

---
+++ index.js
@@ -0,0 +1,13 @@
+//var idade = window.prompt('Digite sua idade');
+// para transformar em em numero use o parseInt
+// var idade = parseInt(window.prompt('Digite sua idade'));
+
+//document.write(idade);
+
+// var isConfirm = window.confirm();
+// document.write(isConfirm);
+// document.write(typeof isConfirm);
+
+var nome = window.prompt('Digite seu nome');
+var sobrenome = window.prompt('Digite seu sobrenome');
+console.log(nome + ' ' + sobrenome);
